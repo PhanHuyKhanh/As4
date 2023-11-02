6d46a7b3b0e9558b8cc7c7b805e599134921b42a
PhanHuyKhanh <144340843+PhanHuyKhanh@users.noreply.github.com>
2023-11-02 18:49:42 +0700

    as4
    as4

---
+++ Assignment 4/bai 12/rating.sql
@@ -0,0 +1,98 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 02, 2023 at 11:51 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `rating`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(10) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `img_url` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `title`, `img_url`) VALUES
+(1, 'MB Gigabyte G1.Sniper B5', 'template/img/products/1.jpg'),
+(2, 'Asus MeMo pad ME102A', 'template/img/products/2.jpg'),
+(3, 'LCD Philips 27\'\' 273G3 DHSW', 'template/img/products/3.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rating_info`
+--
+
+CREATE TABLE `rating_info` (
+  `product_id` int(10) NOT NULL,
+  `rate_1` int(10) NOT NULL,
+  `rate_2` int(10) NOT NULL,
+  `rate_3` int(10) NOT NULL,
+  `rate_4` int(10) NOT NULL,
+  `rate_5` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `rating_info`
+--
+
+INSERT INTO `rating_info` (`product_id`, `rate_1`, `rate_2`, `rate_3`, `rate_4`, `rate_5`) VALUES
+(1, 1, 2, 3, 1, 2),
+(2, 12, 23, 21, 22, 11),
+(3, 2, 2, 1, 1, 2);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `rating_info`
+--
+ALTER TABLE `rating_info`
+  ADD PRIMARY KEY (`product_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
